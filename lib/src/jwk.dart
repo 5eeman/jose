d2d8b1d3befd571d8d2ffaa59e5ef67e7573cd31
Soroush Yousefpour <h.yusefpour@gmail.com>
2025-05-09 14:23:44 +0200

    feat: Updated dependencies: crypto_keys_plus to 0.5.0 and x509_plus to 0.3.3.

--- lib/src/jwk.dart
+++ lib/src/jwk.dart
@@ -9,8 +9,7 @@ import 'dart:convert' as convert;
 import 'dart:typed_data';
 
 import 'package:asn1lib/asn1lib.dart';
-import 'package:collection/collection.dart'
-    show IterableExtension, IterableNullableExtension;
+import 'package:collection/collection.dart' show IterableExtension;
 import 'package:crypto_keys_plus/crypto_keys.dart';
 import 'package:http/http.dart' as http;
 import 'package:http_parser/http_parser.dart';
@@ -442,14 +441,14 @@ class JsonWebKeySet extends JsonObject {
   /// An array of JWK values
   List<JsonWebKey> get keys =>
       getTypedList<JsonWebKey?>('keys', factory: (v) => JsonWebKey.fromJson(v))
-          ?.whereNotNull()
+          ?.nonNulls
           .toList() ??
       const [];
 
   /// Constructs a [JsonWebKeySet] from the list of [keys]
   factory JsonWebKeySet.fromKeys(Iterable<JsonWebKey?> keys) =>
       JsonWebKeySet.fromJson({
-        'keys': keys.map((v) => v?.toJson()).whereNotNull().toList(),
+        'keys': keys.map((v) => v?.toJson()).nonNulls.toList(),
       });
 
   /// Constructs a [JsonWebKeySet] from its JSON representation
